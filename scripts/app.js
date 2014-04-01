787c197174cf69fb0e5ca36b2848859618a18607
thetallweeks <kevinmichaelweeks@gmail.com>
2014-03-31 23:21:54 -0700

    Added simple rss feed

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+angular.module('app', []).
+controller('feedCtrl', ['$scope', '$http', function($scope, $http){
+  var feedUrl = 'http://www.biblegateway.com/votd/get/?format=atom';
+  var url = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(feedUrl);
+  $http.jsonp(url).
+    success(function(data, status, headers, config) {
+      var entry = data.responseData.feed.entries[0];
+      $scope.feed = {
+        title: entry.title,
+        content: entry.content
+      };
+    }).
+    error(function(data, status, headers, config) {
+      console.error('Error fetching feed:', data);
+    });
+}]);
