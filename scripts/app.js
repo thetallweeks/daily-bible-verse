3740a8b2e3ab59be54f4ee72ec20f2469c90937a
Kevin Weeks <keweeks@cisco.com>
2018-01-09 08:45:30 -0800

    Fixed rss feed

--- scripts/app.js
+++ scripts/app.js
@@ -5,26 +5,26 @@
 
   // Data
   angular.module('app').controller('verseCtrl', ['$scope', '$http', function ($scope, $http) {
-    var feedUrl = 'http://www.biblegateway.com/votd/get/?format=atom';
+    var url = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.biblegateway.com%2Fvotd%2Fget%2F%3Fformat%3Datom';
 
-    var url = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(feedUrl);
-    $scope.waitFor = $http.jsonp(url).success(function (data, status, headers, config) {
-      var entry = data.responseData.feed.entries[0];
-      $scope.feed = {
-        title: entry.title,
-        content: entry.content
-      };
-    }).error(function (data, status, headers, config) {
-      console.error('Error fetching feed:', data);
-
-      // Fallback to a good verse
-      $scope.feed = {
-        title: '1 Thessalonians 5:18',
-        content: 'give thanks in all circumstances; for this is God’s will for you in Christ Jesus.'
-      }
-    }).finally(function () {
-      $scope.loaded = true;
-    });
+    $scope.waitFor = $http.get(url)
+      .then(function (response) {
+        var entry = response.data.items[0];
+        $scope.feed = {
+          title: entry.title,
+          content: entry.content
+        };
+      })
+      .catch(function (error) {
+        // Fallback to a good verse
+        $scope.feed = {
+          title: '1 Thessalonians 5:18',
+          content: 'give thanks in all circumstances; for this is God’s will for you in Christ Jesus.'
+        };
+      })
+      .finally(function () {
+        $scope.loaded = true;
+      });
   }]);
 
   // Slideshow
